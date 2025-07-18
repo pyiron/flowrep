886d59a323738bd53108b69c11c4441853681e21
samwaseda <o.waseda@mpie.de>
2025-07-18 08:29:24 +0000

    run update_module_name.sh

--- tests/unit/test_tests.py
+++ tests/unit/test_tests.py
@@ -1,10 +1,10 @@
 import unittest
 
-import pyiron_module_template
+import chefflow
 
 
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        version = pyiron_module_template.__version__
+        version = chefflow.__version__
         print(version)
         self.assertTrue(version.startswith("0"))
