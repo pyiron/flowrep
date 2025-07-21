d9eaabf1d6cdde55e1e5e307fae54c88a56ef91c
Sam Waseda <waseda@mpie.de>
2025-07-21 19:24:56 +0200

    Use semantikon datastructure

--- flowrep/workflow.py
+++ flowrep/workflow.py
@@ -19,7 +19,7 @@ from semantikon.converter import (
     parse_input_args,
     parse_output_args,
 )
-from flowrep.datastructure import (
+from semantikon.datastructure import (
     MISSING,
     CoreMetadata,
     Edges,
