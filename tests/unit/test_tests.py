9b26d57c304e5550e2654f98d30ce1807681fdf5
samwaseda <o.waseda@mpie.de>
2025-07-21 06:50:41 +0000

    run update_module_name.sh

--- tests/unit/test_tests.py
+++ tests/unit/test_tests.py
@@ -1,10 +1,10 @@
 import unittest
 
-import pyiron_module_template
+import flowrep
 
 
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        version = pyiron_module_template.__version__
+        version = flowrep.__version__
         print(version)
         self.assertTrue(version.startswith("0"))
