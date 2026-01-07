2ff08fa52a8673119bcd809347083fe62b7b3b67
liamhuber <liamhuber@greyhavensolutions.com>
2026-01-06 16:01:18 -0800

    Scaffold the two most basic models
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- flowrep/model.py
+++ flowrep/model.py
@@ -1,4 +1,13 @@
 from typing import Literal
+import pydantic
 
-RecipeElementType = Literal["function", "workflow", "for", "while", "try", "if"]
+RecipeElementType = Literal["atomic", "workflow", "for", "while", "try", "if"]
 
+class NodeModel(pydantic.BaseModel):
+    type: RecipeElementType
+
+class AtomicNode(NodeModel):
+    type: Literal["atomic"] = "atomic"
+
+class WorkflowNode(NodeModel):
+    type: Literal["workflow"] = "workflow"
