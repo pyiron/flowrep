3f708c2087ca6b29108efb7aaa8f46cbb4a57292
liamhuber <liamhuber@greyhavensolutions.com>
2026-01-06 16:05:27 -0800

    Store atomic node reference
    I'm lifting the validator directly from @XzzX's [`PythonWorkflowDefinitionFunctionNode`](https://github.com/XzzX/python-workflow-definition/blob/fec059137d5c23a5983a798d347a50dbb911e56b/src/python_workflow_definition/models.py#L57)
    
    Co-authored-by: Sebastian Eibl <xzzx@users.noreply.github.com>
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- flowrep/model.py
+++ flowrep/model.py
@@ -8,6 +8,18 @@ class NodeModel(pydantic.BaseModel):
 
 class AtomicNode(NodeModel):
     type: Literal["atomic"] = "atomic"
+    fully_qualified_name: str
+
+    @pydantic.field_validator("fully_qualified_name")
+    @classmethod
+    def check_name_format(cls, v: str):
+        if not v or "." not in v or v.startswith(".") or v.endswith("."):
+            msg = (
+                f"AtomicNode 'fully_qualified_name' must be a non-empty string ",
+                f"in the format 'module.qualname' with at least one period. Got {v}"
+            )
+            raise ValueError(msg)
+        return v
 
 class WorkflowNode(NodeModel):
     type: Literal["workflow"] = "workflow"
