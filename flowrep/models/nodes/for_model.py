e964debd7e277fcf01e46936c8f16a1c3ebd3bc1
liamhuber <liamhuber@greyhavensolutions.com>
2026-01-22 13:44:34 -0800

    Add alias for label lists
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- flowrep/models/nodes/for_model.py
+++ flowrep/models/nodes/for_model.py
@@ -72,12 +72,8 @@ class ForNode(base_models.NodeModel):
     body_node: helper_models.LabeledNode
     input_edges: edge_models.InputEdges
     output_edges: edge_models.OutputEdges
-    nested_ports: base_models.UniqueList[base_models.Label] = pydantic.Field(
-        default_factory=list
-    )
-    zipped_ports: base_models.UniqueList[base_models.Label] = pydantic.Field(
-        default_factory=list
-    )
+    nested_ports: base_models.Labels = pydantic.Field(default_factory=list)
+    zipped_ports: base_models.Labels = pydantic.Field(default_factory=list)
     transfer_edges: dict[edge_models.OutputTarget, edge_models.InputSource] = (
         pydantic.Field(default_factory=dict)
     )
