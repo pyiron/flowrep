d50f57246d1041a4097f99fc886f0bc3ae4edc57
liamhuber <liamhuber@greyhavensolutions.com>
2026-01-22 10:12:44 -0800

    Relocate rebuild
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

---
+++ flowrep/models/nodes/__init__.py
@@ -0,0 +1,26 @@
+from typing import cast
+
+import pydantic
+
+from flowrep.models.nodes import (
+    atomic_model,
+    for_model,
+    helper_models,
+    if_model,
+    model,
+    try_model,
+    while_model,
+    workflow_model,
+)
+from flowrep.models.nodes.union import NodeType
+
+for cls in [
+    atomic_model.AtomicNode,
+    for_model.ForNode,
+    helper_models.LabeledNode,
+    if_model.IfNode,
+    try_model.TryNode,
+    while_model.WhileNode,
+    workflow_model.WorkflowNode,
+]:
+    cast(type[pydantic.BaseModel], cls).model_rebuild()
