47f47ebebd520a0106ca6db36cb7368f1cf32fdc
liamhuber <liamhuber@greyhavensolutions.com>
2026-01-23 14:00:08 -0800

    Remove unnecessary comment block
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- flowrep/models/nodes/while_model.py
+++ flowrep/models/nodes/while_model.py
@@ -8,13 +8,7 @@ from flowrep.models import base_models, edge_models
 from flowrep.models.nodes import helper_models
 
 if TYPE_CHECKING:
-    from flowrep.models.nodes.union import Nodes  # Satisfies mpypy
-
-    # Still not enough to satisfy ruff, which doesn't understand the string forward
-    # reference, even with the TYPE_CHECKING import
-    # Better to nonetheless leave the references as strings to make sure the pydantic
-    # handling of forward references is maximally robust through the model_rebuild()
-    # Ultimately, just silence ruff as needed
+    from flowrep.models.nodes.union import Nodes
 
 
 class WhileNode(base_models.NodeModel):
