b1110ee4766cbf618017e70c793508e4a69c82b6
Sam Waseda <waseda@mpie.de>
2026-02-24 07:18:59 +0100

    Get rid of what was before

--- tests/unit/test_crawler.py
+++ tests/unit/test_crawler.py
@@ -5,22 +5,6 @@ from pyiron_snippets import versions
 
 from flowrep import crawler
 
-
-def add(x, y):
-    return x + y
-
-
-def op(a, b):
-    c = add(a, b)
-    d = math.sqrt(c)
-    return d
-
-
-def more_op(a, b):
-    c = op(a, b)
-    return c
-
-
 # ---------------------------------------------------------------------------
 # Helper functions defined at module level so they have inspectable source,
 # a proper __module__, and a stable __qualname__.
@@ -90,26 +74,6 @@ def _fqn(func) -> str:
 
 def _fqns(deps: crawler.CallDependencies) -> set[str]:
     return {info.fully_qualified_name for info in deps}
-
-
-class TestCrawler(unittest.TestCase):
-    def test_analyze_function_dependencies(self):
-        loc, ext = crawler.analyze_function_dependencies(op)
-        self.assertEqual(loc, {add})
-        self.assertEqual(len(ext), 1)
-        f = ext.pop()
-        self.assertEqual(f.fully_qualified_name, "math.sqrt")
-        loc, ext = crawler.analyze_function_dependencies(more_op)
-        self.assertEqual(loc, {op, add})
-        self.assertEqual(len(ext), 1)
-        g = ext.pop()
-        self.assertEqual(g.fully_qualified_name, "math.sqrt")
-
-    def test_extract_called_functions(self):
-        called = crawler.extract_called_functions(op)
-        self.assertEqual(called, {add, math.sqrt})
-        called = crawler.extract_called_functions(more_op)
-        self.assertEqual(called, {op})
 
 
 class TestGetCallDependencies(unittest.TestCase):
