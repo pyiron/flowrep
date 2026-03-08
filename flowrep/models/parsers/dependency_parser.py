740aa5162f35fd7d0d701f98f434292a94545bfc
Sam Waseda <waseda@mpie.de>
2026-03-08 22:01:43 +0100

    Create find_undefined_variables

--- flowrep/models/parsers/dependency_parser.py
+++ flowrep/models/parsers/dependency_parser.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import ast
 import builtins
+import inspect
 import types
 from collections.abc import Callable
 
@@ -228,3 +229,60 @@ class CallCollector(ast.NodeVisitor):
         for generator in node.generators:
             self._process_assignment_target(generator.target)
         self.generic_visit(node)
+
+
+class UndefinedVariableVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.used_vars = set()
+        self.defined_vars = set()
+
+    def visit_Name(self, node):
+        if isinstance(node.ctx, ast.Load):  # Variable is being used
+            self.used_vars.add(node.id)
+        elif isinstance(node.ctx, ast.Store):  # Variable is being defined
+            self.defined_vars.add(node.id)
+
+    def visit_FunctionDef(self, node):
+        # Add function arguments to defined variables
+        for arg in node.args.args:
+            self.defined_vars.add(arg.arg)
+        self.generic_visit(node)
+
+
+def find_undefined_variables(
+    func_or_var: Callable | object,
+    version_scraping: versions.VersionScrapingMap | None = None,
+    _call_dependencies: CallDependencies | None = None,
+    _visited: set[str] | None = None,
+) -> set[str]:
+
+    call_dependencies: CallDependencies = _call_dependencies or {}
+    visited: set[str] = _visited or set()
+
+    func_fqn = versions.VersionInfo.of(func_or_var).fully_qualified_name
+    if func_fqn in visited:
+        return call_dependencies
+    visited.add(func_fqn)
+
+    if callable(func_or_var):
+        source = inspect.getsource(func_or_var)
+    else:
+        source = str(func_or_var)
+    tree = ast.parse(source)
+
+    visitor = UndefinedVariableVisitor()
+    visitor.visit(tree)
+
+    # Find variables that are used but not defined
+    scope = object_scope.get_scope(func_or_var)
+    undefined_vars = visitor.used_vars - visitor.defined_vars
+    for item in undefined_vars.difference(set(dir(builtins))):
+        try:
+            obj = object_scope.resolve_attribute_to_object(item, scope)
+        except (ValueError, TypeError):
+            continue
+        info = versions.VersionInfo.of(obj, version_scraping=version_scraping)
+        call_dependencies[info] = obj
+
+        find_undefined_variables(obj, version_scraping, call_dependencies, visited)
+    return call_dependencies
