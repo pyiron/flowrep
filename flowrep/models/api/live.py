d8be38898f9c6f4ef341cb66462020b3c22977ca
liamhuber <liamhuber@greyhavensolutions.com>
2026-03-17 15:47:49 -0700

    Deduplicate API
    Honestly, I'm not sure the `nodes` content should be duplicated in the `schema` either, but at least nodes are a bit of a special(y important) case. For now just don't make things worse, and stick all the schema-like stuff in schemas and leave the user-facing function in its own api module. Overall the api probably needs to re-pathing anyway.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- flowrep/models/api/live.py
+++ flowrep/models/api/live.py
@@ -6,13 +6,4 @@ Since they hold live, arbitrary python objects, they don't serializes trivially 
 not (by themselves) a valid storage format.
 """
 
-from flowrep.models.api.schemas import Atomic as Atomic
-from flowrep.models.api.schemas import Composite as Composite
-from flowrep.models.api.schemas import FlowControl as FlowControl
-from flowrep.models.api.schemas import InputPort as InputPort
-from flowrep.models.api.schemas import LiveNode as LiveNode
-from flowrep.models.api.schemas import NotData as NotData
-from flowrep.models.api.schemas import OutputPort as OutputPort
-from flowrep.models.api.schemas import Workflow as Workflow
-from flowrep.models.live import NOT_DATA as NOT_DATA
 from flowrep.models.live import recipe2live as recipe2live
