25f6fb40cd3e836d7865c4ca212bdaa5a16c7bcc
liamhuber <liamhuber@greyhavensolutions.com>
2026-06-10 10:02:53 -0700

    Make the api the source of truth
    Exposing the tools and schemas right there, and routing the top level improts through just `api`.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

---
+++ src/flowrep/api/__init__.py
@@ -0,0 +1,15 @@
+"""
+The public interface for flowrep, protected by semver
+
+The most fundamental user tools (parsing and decorating) are provided here at the top
+level, while other resources are nested by category as tools or schemas. The shorter
+tools list is likely to be useful for all power users, while schemas are intended to
+primarily benefit downstream developers (e.g. WfMS creators).
+"""
+
+from flowrep.api import schemas as schemas
+from flowrep.api import tools as tools
+from flowrep.api.tools import atomic as atomic
+from flowrep.api.tools import parse_atomic as parse_atomic
+from flowrep.api.tools import parse_workflow as parse_workflow
+from flowrep.api.tools import workflow as workflow
